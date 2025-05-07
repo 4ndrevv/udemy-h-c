367892ed6468fdf5709347a00fdbc0c84970269b
Nam <namhh09110@gmail.com>
2025-05-07 10:14:53 +0200

    win 0705

---
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -0,0 +1,21 @@
+import Player from "./components/Player";
+
+function App() {
+  
+
+  return (
+  <main>
+    <div id="game-container">
+      <ol id="players">
+        <Player initialName="PLAYER 1" symbol="X"/>
+        <Player initialName="PLAYER 2" symbol="O" />
+      </ol>
+      GAME BOARD
+    </div>
+
+    LOG
+  </main>
+  )
+}
+
+export default App
