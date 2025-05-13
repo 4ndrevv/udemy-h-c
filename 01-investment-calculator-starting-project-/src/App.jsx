f639ffe401aa5c817b0e3862a1e62fbd88867820
Hai Nam Hoang <hainamhoang@MacBook-Pro-cua-Hai.local>
2025-05-13 16:04:19 +0200

    mac 1305

--- 01-investment-calculator-starting-project-/src/App.jsx
+++ 01-investment-calculator-starting-project-/src/App.jsx
@@ -1,6 +1,38 @@
+import { useState } from 'react'
+
+import Header from './components/Header.jsx'
+import UserInput from './components/UserInput.jsx'
+import Reasults from './components/Results.jsx'
+
 function App() {
+
+  const[userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+});
+
+const inputIsValid = userInput.duration >= 1;
+
+function handleChange(inputIdentifier ,newValue){
+    setUserInput(prevUserInput => {
+        return {
+            ...prevUserInput,
+            [inputIdentifier]: +newValue
+        }
+    });
+}
+
+
+
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+      <Header />
+      <UserInput onChangeInput={handleChange} userInput={userInput} />
+      { !inputIsValid && <p className='center' >Please enter duration bigger than 0 </p> }
+      { inputIsValid && <Reasults input={ userInput } /> }
+    </>
   )
 }
 
