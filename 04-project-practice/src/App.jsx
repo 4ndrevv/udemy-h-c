1e71c1b7bbadc955514300a8f6602d14de1f7340
Nam <namhh09110@gmail.com>
2025-05-23 10:14:25 +0200

    win 2305

---
+++ 04-project-practice/src/App.jsx
@@ -0,0 +1,14 @@
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectSidebar from "./components/ProjectSideBar";
+
+function App() {
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar />
+      <NoProjectSelected />
+    </main>
+  );
+}
+
+export default App;
---
+++ 04-project-practice/src/components/ProjectSideBar.jsx
@@ -0,0 +1,15 @@
+import Button from './Button.jsx'
+
+export default function ProjectSidebar() {
+    return (
+        <aside className="w-1/3 px-5 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+            <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Project</h2>
+            <div>
+                <Button>
+                    + Add Project
+                </Button>
+                <ul></ul>
+            </div>
+        </aside>
+    );  
+}
