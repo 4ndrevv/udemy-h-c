1849b02ffa47172ec05598f5fb321c81127fef8f
Nam <namhh09110@gmail.com>
2025-05-24 12:58:29 +0200

    win 2405

--- 04-project-practice/src/App.jsx
+++ 04-project-practice/src/App.jsx
@@ -2,11 +2,53 @@ import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSideBar";
 
+import { useState } from "react";
+
 function App() {
+
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+  });
+  }
+
+  function handleAddProject(projectData){
+    setPrrojectState((prevState) => {
+
+      const newProject = {
+        ...projectData,
+        id: Math.random().toString()
+      };
+
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      }
+    });
+  }
+
+  console.log(projectState);
+
+  let content ;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NoProjectSelected />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
