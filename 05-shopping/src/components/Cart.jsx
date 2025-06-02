96fb2c178a80af39806e8e994ad81c2c994e7c68
Nam <namhh09110@gmail.com>
2025-06-03 00:25:33 +0200

    win0306

--- 05-shopping/src/components/Cart.jsx
+++ 05-shopping/src/components/Cart.jsx
@@ -1,4 +1,11 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
+
+import { useContext } from 'react'
+import { CartContext } from "../store/shopping-cart";
+
+export default function Cart() {
+  
+    const {items, updateCartItemQuantity} = useContext(CartContext);
+  
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
@@ -6,6 +13,7 @@ export default function Cart({ items, onUpdateItemQuantity }) {
   const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
 
   return (
+
     <div id="cart">
       {items.length === 0 && <p>No items in cart!</p>}
       {items.length > 0 && (
@@ -20,11 +28,11 @@ export default function Cart({ items, onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => updateCartItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateCartItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
